34201c58de63dd6a51b907c0c3e37b87ba1438f6
Uy Pham <uyphamvuong@gmail.com>
2026-04-11 16:14:48 +0700

    feat: add new analytics block

--- gia-vang.js
+++ gia-vang.js
@@ -30,11 +30,15 @@ const getNgay = () => {
 
 const getGiaVang = () => {
   const $goldMain = document.getElementById("gia-vang");
+  const $goldAnalytic = document.getElementById("gia-vang-analytic");
 
   fetch("https://edge-cf-api.pnj.io/ecom-frontend/v1/get-gold-price?zone=21")
     .then((res) => res.json())
     .then((data) => {
       const goldTableData = data.data;
+      const vangSjc = goldTableData.find((item) => item.masp === "999");
+      $goldAnalytic.innerHTML = `<h2>Vàng</h2><div>Mua: <span>${formatNumber(vangSjc.giamua)}</span></div>
+        <div>Bán: <span>${formatNumber(vangSjc.giaban)}</span></div>`;
 
       // Bảng giá vàng
       const $goldTable = document.createElement("table");
@@ -42,8 +46,8 @@ const getGiaVang = () => {
       $goldThead.innerHTML = `<tr class="yellow">
         <th>Mã SP</th>
         <th>Tên SP</th>
-        <th class="right">Mua</th>
-        <th class="right">Bán</th>
+        <th class="right">Mua (K)</th>
+        <th class="right">Bán (K)</th>
       </tr>`;
       $goldTable.append($goldThead);
       const $goldTbody = document.createElement("tbody");
@@ -65,14 +69,24 @@ const getGiaVang = () => {
     .catch((err) => {
       console.error("Lỗi khi lấy giá vàng:", err);
       $goldMain.innerHTML = "<span class='red'>Không thể tải giá vàng.</span>";
+      $goldAnalytic.innerHTML =
+        "<h2>Vàng</h2><div class='red'>Không thể tải giá vàng.</div>";
     });
 };
 
 const getGiaBac = () => {
   const $silverMain = document.getElementById("gia-bac");
+  const $silverAnalytic = document.getElementById("gia-bac-analytic");
+
   fetch("https://apiweb.cafef.vn/api/v1/Silver/GetSilver")
     .then((res) => res.json())
     .then((silverTableData) => {
+      const bacSjc = silverTableData.find(
+        (item) => item.name === "Bạc thỏi Phú Quý 999 1Kilo",
+      );
+      $silverAnalytic.innerHTML = `<h2>Bạc</h2><div>Mua: <span>${formatNumber(bacSjc.buyPrice / 1000)}</span></div>
+        <div>Bán: <span>${formatNumber(bacSjc.sellPrice / 1000)}</span></div>`;
+
       // Bảng giá bạc
       const $silverTable = document.createElement("table");
       const $silverThead = document.createElement("thead");
@@ -100,6 +114,93 @@ const getGiaBac = () => {
     .catch((err) => {
       console.error("Lỗi khi lấy giá bạc:", err);
       $silverMain.innerHTML = "<span class='red'>Không thể tải giá bạc.</span>";
+      $silverAnalytic.innerHTML =
+        "<h2>Bạc</h2><div class='red'>Không thể tải giá bạc.</div>";
+    });
+};
+
+const getGiaNgoaiTe = () => {
+  const $currencyMain = document.getElementById("gia-ngoai-te");
+  const $currencyAnalytic = document.getElementById("gia-usd-analytic");
+
+  const today = new Date();
+  const dateStr = `${String(today.getDate()).padStart(
+    2,
+    "0",
+  )}/${String(today.getMonth() + 1).padStart(2, "0")}/${today.getFullYear()}`;
+  fetch(
+    `https://cafef.vn/du-lieu//ajax/exchangerate/ajaxratecurrency.ashx?time=${dateStr}`,
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const currencyTableData = data.Data;
+
+      const usd = currencyTableData.find((item) => item.currencyName === "USD");
+      $currencyAnalytic.innerHTML = `<h2>USD</h2><div>Mua: <span>${formatNumber(usd.buyCash)} đ</span></div>
+        <div>Bán: <span>${formatNumber(usd.price)} đ</span></div>`;
+
+      // Bảng giá ngoại tệ
+      const $currencyTable = document.createElement("table");
+      const $currencyThead = document.createElement("thead");
+      $currencyThead.innerHTML = `<tr>
+        <th>Mã</th>
+        <th>Tên</th>
+        <th class="right">Mua (đ)</th>
+        <th class="right">Bán (đ)</th>
+        <th class="right">Chuyển (đ)</th>
+      </tr>`;
+      $currencyTable.append($currencyThead);
+      const $currencyTbody = document.createElement("tbody");
+      currencyTableData.forEach((item) => {
+        const $html = `<tr>
+          <td class="bold blue">${item.currencyName}</td>
+          <td class="bold blue">${item.name}</td>
+          <td class="right num bold">${formatNumber(item.buyCash)}</td>
+          <td class="right num bold">${formatNumber(item.price)}</td>
+          <td class="right num bold">${formatNumber(item.purchaseTransfer)}</td>
+         </tr>`;
+        const $row = document.createElement("tr");
+        $row.innerHTML = $html;
+        $currencyTbody.append($row);
+      });
+      $currencyTable.append($currencyTbody);
+      $currencyMain.innerHTML = "";
+      $currencyMain.append($currencyTable);
+    })
+    .catch((err) => {
+      console.error("Lỗi khi lấy giá ngoại tệ:", err);
+      $currencyMain.innerHTML =
+        "<span class='red'>Không thể tải giá ngoại tệ.</span>";
+      $currencyAnalytic.innerHTML =
+        "<h2>USD</h2><div class='red'>Không thể tải giá ngoại tệ.</div>";
+    });
+};
+
+const getTinTuc = () => {
+  const $newsMain = document.getElementById("tin-tuc");
+  fetch(
+    "https://cafef.vn/du-lieu/ajax/GoldNews/GoldRelNews.ashx?Type=NEWS&PageIndex=1&PageSize=10",
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const newsData = data.Data;
+
+      const $newsList = document.createElement("div");
+      newsData.forEach((item) => {
+        const $html = `<details>
+        <summary>${item.Title}</summary>
+        ${item.SubContent}
+      </details>`;
+        const $row = document.createElement("div");
+        $row.innerHTML = $html;
+        $newsList.append($row);
+      });
+      $newsMain.innerHTML = "";
+      $newsMain.append($newsList);
+    })
+    .catch((err) => {
+      console.error("Lỗi khi lấy tin tức:", err);
+      $newsMain.innerHTML = "<span class='red'>Không thể tải tin tức.</span>";
     });
 };
 
@@ -109,3 +210,11 @@ setInterval(getNgay, 1000);
 
 getGiaVang();
 getGiaBac();
+getGiaNgoaiTe();
+getTinTuc();
+
+setInterval(() => {
+  getGiaVang();
+  getGiaBac();
+  getGiaNgoaiTe();
+}, 30000);
